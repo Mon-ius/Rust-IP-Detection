14d17468e0dd30c00bfa35babf2bfee5bc77639f
Monius <m1105930979@gmail.com>
2025-07-27 02:15:06 +0800

    fix ppcle

--- src/cli.rs
+++ src/cli.rs
@@ -1,16 +1,16 @@
 use clap::Parser;
 
-#[cfg(all(target_env = "musl", target_pointer_width = "64", not(any(target_arch = "powerpc64"))))]
+#[cfg(all(target_env = "musl", target_pointer_width = "64", not(any(target_arch = "powerpc64", target_arch = "powerpc"))))]
 use jemallocator::Jemalloc;
 
-#[cfg(any(not(all(target_env = "musl")), any(target_arch = "powerpc64")))]
+#[cfg(any(not(all(target_env = "musl")), any(target_arch = "powerpc64", target_arch = "powerpc")))]
 use mimalloc::MiMalloc;
 
-#[cfg(all(target_env = "musl", target_pointer_width = "64", not(any(target_arch = "powerpc64"))))]
+#[cfg(all(target_env = "musl", target_pointer_width = "64", not(any(target_arch = "powerpc64", target_arch = "powerpc"))))]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
-#[cfg(any(not(all(target_env = "musl")), any(target_arch = "powerpc64")))]
+#[cfg(any(not(all(target_env = "musl")), any(target_arch = "powerpc64", target_arch = "powerpc")))]
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
